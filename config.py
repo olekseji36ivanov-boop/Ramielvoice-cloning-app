1a060ccebbb13c400d52ad0dcd5cc64fa021690c
olekseji36ivanov-boop <olekseji36ivanov@gmail.com>
2026-02-27 02:39:43 +0200

    Added configuration for ElevenLabs API and local directory paths

---
+++ config.py
@@ -0,0 +1,9 @@
+# Configuration for ElevenLabs API and local directory paths
+
+E_LABS_API_URL = "https://api.elevenlabs.io/v1/"
+E_LABS_API_KEY = "your_api_key_here"
+
+# Local directory paths
+AUDIO_OUTPUT_DIR = "./audio_outputs/"
+MODEL_DIR = "./models/"
+TEMPLATES_DIR = "./templates/"
